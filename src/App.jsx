0d410dc982cd58be0ec52574aa22afbc24b0ab4a
Tushar Khushal Mahajan <mahajantushar208@gmail.com>
2025-01-29 12:10:10 +0530

    create frontend for creating and joining room

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,35 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { toast } from 'react-hot-toast';
+
+
+// import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <div className='min-h-screen bg-gray-950 text-gray-200 flex items-center justify-center'>
+      <section className='bg-gray-900 p-8 w-full max-w-md border border-gray-800 shadow flex flex-col gap-4'>
+        <h1 className='text-2xl font-semibold text-center'>Create or Join Room</h1>
+
+        <div className='text-lg'>
+          <label htmlFor="NAME" className='block'>Your Name</label>
+          <input type="text" name="name" id="NAME" placeholder='Enter your name' className='mt-1 border-2 border-gray-600 rounded-full px-2 w-full bg-gray-800' />
+        </div>
+
+        <div className='text-lg'>
+          <label htmlFor="ROOOM_ID" className='block'>Room Id</label>
+          <input type="text" name="rommId" id="ROOOM_ID" placeholder='Enter Room ID' className='mt-1 border-2 border-gray-600 rounded-full px-2 w-full bg-gray-800' />
+        </div>
+
+        <div className='mt-1 flex justify-end text-gray-950 font-semibold'>
+          <button className='bg-orange-500 px-3 py-1.5 rounded-full hover:bg-orange-600'>
+            Join Room
+          </button>
+          <button className='ms-3 bg-blue-500 px-3 py-1.5 rounded-full hover:bg-blue-600'>
+            Create Room
+          </button>
+        </div>
+      </section>
+    </div>
   )
 }
 
