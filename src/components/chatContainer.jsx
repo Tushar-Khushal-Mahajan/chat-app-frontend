72524363a6c5e87933d723db47254a54de6a272f
Tushar Khushal Mahajan <mahajantushar208@gmail.com>
2025-01-29 19:43:02 +0530

    remote static messages and use map insted..

--- src/components/chatContainer.jsx
+++ src/components/chatContainer.jsx
@@ -1,41 +1,57 @@
+import { useState } from "react";
 import ChatSend from "./chatSend";
+
 
 const ChatContainer = () => {
 
+  const [messages, setMessage] = useState([
+    {
+      content: "hii",
+      sender: "Tushar"
+    },
+    {
+      content: "Hello",
+      sender: "Tushar"
+    },
+    {
+      content: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Perspiciatis ipsa inventore quibusdam illo fuga ullam laboriosam nemo, aut praesentium voluptatibus, hic recusandae et earum voluptate nihil, consequuntur eveniet quidem mollitia!",
+      sender: "Tushar"
+    },
+    {
+      content: "hii",
+      sender: "Bhuvan"
+    },
+    {
+      content: "h",
+      sender: "Tushar"
+    }
+  ]);
+  const [input, setInput] = useState("");
+  const inputRef = (null);
+  const chatBoxRef = (null);
+  const [stompClient, setStompClient] = useState(null);
+  const [roomId, setRoomId] = useState("");
+  const [user, setUser] = useState("Tushar");
+
+
   return <>
     <div className="min-w-screen flex justify-center overflow-y-scroll flex-1">
-      <div className="md:min-w-3xl md:text-xl sm:text-xs md:max-w-3xl min-w-full border border-gray-500 p-3 bg-gray-600 text-white flex flex-1 flex-col gap-2 overflow-y-scroll ">
-        <div className="w-full">
-          <section className="p-2 pb-3 bg-white text-black w-fit max-w-5/6 md:max-w-4/6 rounded-bl-2xl rounded-br-2xl rounded-tl-2xl relative   ">
-            <small className="text-xs text-orange-900">Tushar Mahajan</small><br />
-            <span>Hii</span><br />
-            <small className="text-xs text-gray-400 p-0 m-0 absolute right-1 bottom-0">03:27 pm</small>
-          </section>
-        </div>
-        <div className="w-full">
-          <section className="p-2 pb-3 bg-[#D9FDD3] text-black w-fit max-w-5/6 md:max-w-4/6 rounded-bl-2xl rounded-br-2xl rounded-tl-2xl float-right relative ">
-            <small className="text-xs text-orange-900">Tushar Mahajan</small><br />
-            <span>Hii</span><br />
-            <small className="text-xs text-gray-400 p-0 m-0 absolute right-1 bottom-0">03:27 pm</small>
-          </section>
-        </div>
-        <div className="w-full">
-          <section className="p-2 pb-3 bg-[#D9FDD3] text-black w-fit max-w-5/6 md:max-w-4/6 rounded-bl-2xl rounded-br-2xl rounded-tl-2xl float-right relative ">
-            <small className="text-xs text-orange-900">Tushar Mahajan</small><br />
-            <span>Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatum, saepe ex quo totam minus ipsa veniam, commodi ratione laborum blanditiis id tempore fuga tempora eligendi fugiat. Nulla repellat architecto perspiciatis.</span><br />
-            <small className="text-xs text-gray-400 p-0 m-0 absolute right-1 bottom-0">03:27 pm</small>
-          </section>
-        </div>
-        <div className="w-full">
-          <section className="p-2 pb-3 bg-white text-black w-fit max-w-5/6 md:max-w-4/6 rounded-bl-2xl rounded-br-2xl rounded-tl-2xl relative   ">
-            <small className="text-xs text-orange-900">Tushar Mahajan</small><br />
-            <span>Lorem ipsum dolor</span><br />
-            <small className="text-xs text-gray-400 p-0 m-0 absolute right-1 bottom-0">03:27 pm</small>
-          </section>
-        </div>
+      <div className="md:min-w-3xl md:text-xl sm:text-xs md:max-w-3xl min-w-full border border-gray-500 p-3 bg-gray-600 text-white flex flex-1 flex-col gap-2 overflow-y-scroll">
+        {messages.map((message, index) => (
+          <div key={index} className="w-full">
+            <section
+              className={`p-2 pb-3 
+            ${(message.sender === user) ? 'bg-[#D9FDD3] text-black float-right' : 'bg-white text-black'} 
+            min-w-[8rem] max-w-5/6 md:max-w-4/6 rounded-bl-2xl rounded-br-2xl rounded-tl-2xl relative inline-block`}
+            >
+              <small className="text-xs text-orange-900">{message.sender}</small><br />
+              <span className="text-[0.8em]">{message.content}</span><br />
+              <small className="text-xs text-gray-400 p-0 m-0 absolute right-1 bottom-0">03:27 pm</small>
+            </section>
+          </div>
+        ))}
       </div>
     </div>
-
   </>;
 }
 
